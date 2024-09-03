ea4ce2c71d3ba6b0e008188a0fd45f4e2662b4ea
Robert Crocker <rcrocker13@gmail.com>
2024-09-02 22:03:42 -0500

    'Add Prettier and Tailwind CSS configuration files, update package dependencies. (69)'

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutRoute() {
+  return (
+    <main>
+      <h1 className="text-4xl font-bold">About</h1>
+      <p className="mt-5">This is the about page</p>
+    </main>
+  );
+}
