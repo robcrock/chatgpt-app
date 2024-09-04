ddd534ed986259b34495e7d6924307f5d0064f2e
Robert Crocker <rcrocker13@gmail.com>
2024-09-03 21:43:43 -0500

    'Introduce code style and structure guidelines, update package dependencies. (68)'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import { SessionProvider } from "@/app/components/SessionProvider";
+import UserButton from "./components/UserButton";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,23 +18,27 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} px-2 md:px-5`}>
-        <header className="mb-3 flex rounded-b-lg bg-green-900 p-2 text-2xl font-bold text-white shadow-lg shadow-gray-700">
-          <div className="flex flex-grow">
-            <Link href="/" className="font-bold">
-              GPT Chat
-            </Link>
-            <Link href="/about" className="ml-5 font-light">
-              About
-            </Link>
+    <SessionProvider>
+      <html lang="en">
+        <body className={`${inter.className} px-2 md:px-5`}>
+          <header className="mb-3 flex rounded-b-lg bg-green-900 p-2 text-2xl font-bold text-white shadow-lg shadow-gray-700">
+            <div className="flex flex-grow">
+              <Link href="/" className="font-bold">
+                GPT Chat
+              </Link>
+              <Link href="/about" className="ml-5 font-light">
+                About
+              </Link>
+            </div>
+            <div>
+              <UserButton />
+            </div>
+          </header>
+          <div className="flex flex-col md:flex-row">
+            <div className="flex-grow">{children}</div>
           </div>
-          <div></div>
-        </header>
-        <div className="flex flex-col md:flex-row">
-          <div className="flex-grow">{children}</div>
-        </div>
-      </body>
-    </html>
+        </body>
+      </html>
+    </SessionProvider>
   );
 }
