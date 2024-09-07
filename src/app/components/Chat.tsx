41c8563c8b3077740faaa81baaf220870bac757f
Robert Crocker <rcrocker13@gmail.com>
2024-09-06 23:13:10 -0500

    Interactivity

---
+++ src/app/components/Chat.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { getCompletion } from "@/app/server-actions/getCompletion";
+
+import { useState } from "react";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+interface Message {
+  role: "user" | "assistant";
+  content: string;
+}
+
+export default function Chat() {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [message, setMessage] = useState("");
+
+  const onClick = async () => {
+    const completions = await getCompletion([
+      ...messages,
+      {
+        role: "user",
+        content: message,
+      },
+    ]);
+    setMessage("");
+    setMessages(completions.messages);
+  };
+
+  return (
+    <div className="flex flex-col">
+      {messages.map((message, i) => (
+        <div
+          key={i}
+          className={`mb-5 flex flex-col ${
+            message.role === "user" ? "items-end" : "items-start"
+          }`}
+        >
+          <div
+            className={`${
+              message.role === "user" ? "bg-blue-500" : "bg-gray-500 text-black"
+            } rounded-md px-8 py-2`}
+          >
+            {message.content}
+          </div>
+        </div>
+      ))}
+      <div className="mt-3 flex border-t-2 border-t-gray-500 pt-3">
+        <Input
+          className="flex-grow text-xl"
+          placeholder="Question"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyUp={(e) => {
+            if (e.key === "Enter") {
+              onClick();
+            }
+          }}
+        />
+        <Button onClick={onClick} className="ml-3 text-xl">
+          Send
+        </Button>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import { Separator } from "@/components/ui/separator";
+import Chat from "./components/Chat";
+import { getServerSession } from "next-auth";
+
+export default async function Home() {
+  const session = await getServerSession();
+
+  console.log(session);
+
   return (
     <main className="p-5">
       <h1 className="text-4xl font-bold">Welcome To GPT Chat</h1>
+      {!session?.user?.name && <div>You need to log in to use this chat.</div>}
+      {session?.user?.name && (
+        <>
+          <Separator className="my-5" />
+          <Chat />
+        </>
+      )}
     </main>
   );
 }
